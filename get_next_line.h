9673536130ca98ef7d7d021a044b871373045784
Ryosuke Mori <ryomori@c2r2s1.42tokyo.jp>
2024-05-14 15:00:08 +0900

    a

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ryomori <ryomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 11:27:57 by ryomori           #+#    #+#             */
-/*   Updated: 2024/05/14 14:50:34 by ryomori          ###   ########.fr       */
+/*   Updated: 2024/05/14 14:59:26 by ryomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
@@ -27,7 +28,4 @@ char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strchr(const char *s, int c);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*get_next_line(int fd);
-static char	*get_result(int nl_num, char **buffer_s);
-static char	*rd_file(int fd, char **buffer_save, char *buf);
-static void	free_buffer(char **buffer);
 #endif
